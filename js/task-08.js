f31de989892f6d8d1d00cbe9fef456adcee55d95
AlinaKalen <alina103599@gmail.com>
2023-09-21 14:58:05 +0100

    upd

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
 
+
+
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', function (event) { event.preventDefault();
+  
+    const email = this.elements.email.value;
+  const password = this.elements.password.value;
+
+  if (email.trim() === '' || password.trim() === '') {
+    alert('Всі поля повинні бути заповнені!');
+    return; }
+
+    const formData = {  email, password, };
+
+
+    console.log(formData);
+    this.reset();
+});
